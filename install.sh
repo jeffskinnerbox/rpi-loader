f6b0d494a5f4b9b823d314f610761d214719e4be
jeffskinnerbox <jeff.irland@verizon.net>
2017-09-03 11:43:27 -0400

    beginings of a script to suppport moving rpi-loader from one computer to another

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Maintainer:   jeffskinnerbox@yahoo.com / www.jeffskinnerbox.me
+# Version:      0.1
+#
+# DESCRIPTION:
+#
+# NOTE:
+
+
+
+############################ ############################
+
+# directory for where rpi-loader is installed
+ROOT="/home/jeff/src/rpi-loader"
+
+source "$ROOT/ansi.sh"
+source "$ROOT/functions.sh"
+
+TMP="/tmp"
+BOOT="dummy-value"
+DATA="dummy-value"
+LOCAL="$ROOT/rpi3"
+ANS="dummy-value"
+
+############################ ############################
+
+sed 's/ROOT="\/home\/jeff\/src\/rpi-loader"/ROOT=\/home\/pi\/src\/rpi-loader/' part-1.sh
+sed 's/ROOT="\/home\/jeff\/src\/rpi-loader"/ROOT=\/home\/pi\/src\/rpi-loader/' part-2.sh
