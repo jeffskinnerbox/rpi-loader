2911a601ec8fe0fd4a82e4f277a19f7a3b058b37
jeffskinnerbox <jeff.irland@verizon.net>
2017-11-04 15:21:13 -0400

    including additional packages for gstreamer1.0 tools

--- part-5.sh
+++ part-5.sh
@@ -84,7 +84,7 @@ messme "\nInstalling tools to view and manipulate images and video.\n"
 apt-get $OPTS install imagemagick feh mplayer2
 
 # install gstreamer
-apt-get $OPTS install libgstreamer1.0-0-dbg gstreamer1.0-tools libgstreamer-plugins-base1.0-0 gstreamer1.0-plugins-good gstreamer1.0-plugins-bad-dbg gstreamer1.0-omx gstreamer1.0-alsa
+apt-get $OPTS install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools
 
 ############################ ############################
 
